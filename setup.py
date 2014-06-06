9a6bef745ca1ad1e03bd79173558e09f85e837c0
Damian Braun <brunek5252@gmail.com>
2014-06-06 14:13:15 +0200

    first setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from __future__ import print_function
+from setuptools import setup
+
+setup(
+    name='nominatim',
+    version='0.1',
+    author='Damian Braun',
+    author_email='brunek5252@gmail.com',
+    description='OSM Nominatim API module',
+    packages=['nominatim'],
+    platforms='any')
